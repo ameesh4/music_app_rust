cdc8ed0971090b9e69ae7c0c2fbb57c4ea604040
root <root@DESKTOP-S605RPM>
2024-10-22 21:25:25 +0545

    added app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
+import Main from '@/pages/Main';
 import Signin from '@/pages/Signin';
 import Signup from './pages/Signup';
 
@@ -7,7 +7,7 @@ export default function App(){
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
